d079e7505b7594e7172bab278b165565554f8c40
Aakankshachaudhary <aakanksha352005@gmail.com>
2026-09-15 11:45:32 +0530

    feat:configure TravelBharat React routing.

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import States from "./pages/States";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/states" element={<States />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/pages/States.jsx
@@ -0,0 +1,7 @@
+function States() {
+  return (
+    <h1>Explore Indian States</h1>
+  );
+}
+
+export default States;
